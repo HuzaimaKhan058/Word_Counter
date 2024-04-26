a83619e39ecf6590843359917eddfa3e562aaa65
Huzaima Khan <159675886+HuzaimaKhan058@users.noreply.github.com>
2024-04-26 23:48:09 +0500

    A cli based word counter.

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        message: "Please enter a sentence or paragraph:",
+        type: "input"
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+console.log(`Word Count: ${words.length}`);
